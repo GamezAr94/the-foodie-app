9da4d3a8844f26a94d9cd9451d6b35faa77ac527
arturo <arturogamez51@gmail.com>
2024-03-24 16:10:29 -0700

    converting the next app in a PWA
    learning how to convert the next.js into a PWA

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,20 @@ import MainHeader from "@/app/components/main-header/main-header";
 export const metadata = {
     title: "NextLevel Food",
     description: "Delicious meals, shared by a food-loving community.",
+    generator: "Next.js",
+    manifest: "/manifest.json",
+    keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
+    authors: [
+        { name: "Arturo Gamez" },
+        {
+            name: "Arturo Gamez",
+            url: "https://arturogamez.com/",
+        },
+    ],
+    icons: [
+        { rel: "apple-touch-icon", url: "/icons/apple-icon-114x114.png" },
+        { rel: "icon", url: "/icons/apple-icon-144x144.png" },
+    ],
 };
 
 export default function RootLayout({ children }) {
